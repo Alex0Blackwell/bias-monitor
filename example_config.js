1f193ea2fa901c5b1f69601abb8bd02e9a9c6af4
Dylan Wong <dpwong88@gmail.com>
2021-09-18 14:04:05 -0700

    fixed config

--- example_config.js
+++ example_config.js
@@ -1,5 +1,5 @@
 module.exports = {
   environment: "dev",
-  api_url: "https://api.thebipartisanpress.com/api/endpoints/beta/robert",
-  api_key: "gAAAAABeVpQJKRM5BqPX91XW2AKfz8pJosk182maAweJcm5ORAkkBFj__d2feG4H5KIeOKFyhUVSY_uGImiaSBCwy2L6nWxx4g==",
+  api_url: "<your-api-url-endpoint-here>",
+  api_key: "<your-api-key-here>",
 }
