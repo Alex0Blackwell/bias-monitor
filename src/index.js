0670751d0fb7495c0d4215ebf69024a46f67b8ea
alex blackwell <alex.blackwell314@gmail.com>
2021-10-06 19:49:56 -0700

    Update diversity score

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import UiService from "./services/ui_service";
 
-const absoluteMaxScore = 42;
-const maxDiversityScore = 100;
+const abs_max_score = 42;
+const max_diversiry_score = 100;
 
 async function on_start() {
 
@@ -19,24 +19,17 @@ async function on_start() {
         });
 
         if (result.activeScore) {
-            console.log("bias score: " + result.activeScore);
             const normalized_number = UiService.normalize_number(result.activeScore);
             const political_lean = UiService.get_political_lean(normalized_number);
 
-            // Calculating the diversity score
-            const abs_current_score = Math.abs(result.activeScore);
-            console.log("a", abs_current_score)
-            const abs_average_score = Math.abs(result.averageScore);
-            console.log("avg", abs_average_score)
-            const this_website_difference = Math.abs(abs_current_score - abs_average_score);
-            console.log("this diff", this_website_difference)
-            const difference = Math.abs(absoluteMaxScore - this_website_difference);
-            console.log("diff", difference)
-            let diversityScore = maxDiversityScore * (1 - (difference/absoluteMaxScore));
-            diversityScore = Math.ceil(diversityScore/5)*5;
+            const page_score = result.activeScore;
+            const avg_score = result.averageScore;
+            const abs_change = Math.abs((abs_max_score + page_score) - (abs_max_score + avg_score));
 
+            let diversity_score = max_diversiry_score * (abs_change/(abs_max_score*2));
+            diversity_score = Math.ceil(diversity_score/5)*5;
 
-            UiService.update_ui(normalized_number, political_lean, diversityScore);
+            UiService.update_ui(normalized_number, political_lean, diversity_score);
         }
     });
 }
