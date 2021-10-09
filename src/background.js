5935165836d45e9e29d761fea56e094272219f8f
alex blackwell <alex.blackwell314@gmail.com>
2021-10-09 08:56:00 -0700

    Optimize is news site check

--- src/background.js
+++ src/background.js
@@ -4,37 +4,30 @@ chrome.tabs.onActivated.addListener(async (activeInfo) => {
     lastFocusedWindow: true
     }, async function(tabs) {
         const tab_id = activeInfo.tabId;
-        const tab = tabs[0];
+        const url_info = new URL(tabs[0].url)
+        const base_url = url_info.hostname.replace("www.", "");
         let is_news_site = false;
+        let files_to_execute;
 
-        const response = await fetch('newsites.txt');
-        const text = await response.text();
-        const siteURLs = text.split('\n');
+        const news_urls_response = await fetch('news_sites.csv');
+        const news_urls_text = await news_urls_response.text();
+        const all_news_urls = new Set(news_urls_text.split('\n'));
 
-        const num_of_sites = siteURLs.length;
-        let i = 0;
-        while(!is_news_site && i < num_of_sites) {
-            if (tab.url.includes(siteURLs[i++].trim())) {  
-                is_news_site = true;
-            }
+        is_news_site = all_news_urls.has(base_url);
+
+        if(is_news_site) {
+            console.debug(base_url, "is a news website");
+            files_to_execute = ["./scrape.js"];
+        } else {
+            console.debug(base_url, "is not a news website");
+            files_to_execute = ["./not_news_event.js"];
         }
-
         try {
-            if (is_news_site) {
-                console.log("is news")
-                await chrome.scripting.executeScript({
-                    target: { tabId: tab_id },
-                    files: ["./scrape.js"]
-                });
-            } 
-            else {
-                console.log("is not news")
-                await chrome.scripting.executeScript({
-                    target: { tabId: tab_id },
-                    files: ["./not_news_event.js"]
-                });
-            }
-        } catch (error) {
+            await chrome.scripting.executeScript({
+                target: { tabId: tab_id },
+                files: files_to_execute,
+            });
+        } catch(error) {
             console.log(error)
         }
     });
